eb59a121aa4cd31eb179b0f712812fb313730bb6
Jenief <89827478+Jenief@users.noreply.github.com>
2022-03-16 14:52:01 +0530

    Update Remove_word_from_strings.c

--- Remove_word_from_strings.c
+++ Remove_word_from_strings.c
@@ -1,1 +1,35 @@
-# Cprogram
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void removeChar(char * str, char charToRemmove){
+    int i, j;
+    int len = strlen(str);
+   
+    
+}
+
+void main(){
+	 char str[100],ch,c;
+	 int i,j,len;
+	 printf("enter the string : \n");
+	 scanf("%[^\n]%c",str,&c);
+	 printf("enter a char you want to remove : ");
+	 scanf("%c",&ch);
+	 //removeChar(str,ch);
+	 len =strlen(str);
+	 for(i=0;i<len;i++)
+	 {
+	     if(str[i] == ch)
+	     {
+	         for(j=i;j<len;j++)
+	         {
+	             str[j]=str[j+1];
+	         }
+	         len--;
+	         i--;
+	     }
+	 }
+	 printf("String after removing '%c': %s", ch, str);
+	 //getch();
+ } 
